1a4fea1c7d4c6635b0bf064469e319d2255a0fae
Naga <nvijayap@gmail.com>
2019-03-12 10:21:22 -0700

    cleanup

--- users_local.sh
+++ users_local.sh
@@ -9,6 +9,12 @@ if [ $? -ne 0 ]; then
  echo -e "\njson-server --watch data.json\n"; exit 1
 fi
 
+# NOTE: data.json figures in .gitignore
+if [ ! -f data.json ]; then
+  git clone https://github.com/typicode/jsonplaceholder
+  cp jsonplaceholder/data.json .
+fi
+
 sed "s|https://jsonplaceholder.typicode.com|http://localhost:3000|" users.robot > users_local.robot
 
 robot users_local.robot
