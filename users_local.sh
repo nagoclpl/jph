507d7db0ea2b404a92c206fbe72bc1601c6553ae
Naga <nvijayap@gmail.com>
2019-03-12 10:31:24 -0700

    .gitignore, users_local.sh

--- users_local.sh
+++ users_local.sh
@@ -6,13 +6,15 @@ nc -zv localhost 3000 >/dev/null 2>&1
 
 if [ $? -ne 0 ]; then
  echo -e "\nPlease start json server in a separate terminal window using the command below"
- echo -e "\njson-server --watch data.json\n"; exit 1
+ echo -e "\njson-server --watch jsonplaceholder/data.json\n"; exit 1
 fi
 
-# NOTE: data.json figures in .gitignore
-if [ ! -f data.json ]; then
-  git clone https://github.com/typicode/jsonplaceholder
-  cp jsonplaceholder/data.json .
+# NOTE 1: data.json figures in .gitignore
+# NOTE 2: As data.json may change when running the test, we get it every time before running test
+if [ -d jsonplaceholder ]; then
+  (cd jsonplaceholder; git pull)
+else
+  git clone https://github.com/typicode/jsonplaceholder.git
 fi
 
 sed "s|https://jsonplaceholder.typicode.com|http://localhost:3000|" users.robot > users_local.robot
