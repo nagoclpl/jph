2bbaf6cfccc335cff3fb50413bc6d9308f12022d
Naga <nvijayap@gmail.com>
2019-03-14 11:31:18 -0700

    open report.html

--- users_local.sh
+++ users_local.sh
@@ -4,7 +4,12 @@
 
 echo; nc -zv localhost 3000 >/dev/null 2>&1; if [ $? -eq 0 ]; then robot users_local.robot; else
  echo -e "Please start json server in a separate terminal window using the commands below"
- echo -e "\ncd ~/jsonplaceholder\n"; echo -e "\nnode index.js\n"; exit 1
+ echo -e "\ncd ~/jsonplaceholder\n"; echo -e "npm start\n"; exit 1
 fi
 
 robot users_local.robot
+
+if [ "`uname -s`" == "Darwin" ]; then
+  type realpath && open file://`realpath report.html` || \
+    echo -e "\nPlease brew install coreutils\n"
+fi
