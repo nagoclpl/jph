f6d95a83799d91a1c23129ba763984b833062fcd
Naga <nvijayap@gmail.com>
2019-03-13 20:38:28 -0700

    .gitignore, hw.py, hw.sh

---
+++ flask/hw.py
@@ -0,0 +1,8 @@
+# hw.py
+
+from flask import Flask
+app = Flask(__name__)
+
+@app.route("/")
+def hello():
+  return "Hello World!"
---
+++ flask/hw.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/env bash
+
+# hw.sh
+
+export FLASK_ENV=development
+
+export FLASK_APP=hw.py
+
+flask run
