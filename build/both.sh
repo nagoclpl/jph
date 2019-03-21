0d70ce3e5d87b121915f3f153aa793627b7c72a7
Naga <nagoclpl@gmail.com>
2019-03-21 12:03:57 -0700

    cleanup

--- build/both.sh
+++ build/both.sh
@@ -1,6 +1,15 @@
 #!/usr/bin/env bash
 
-echo; for ans in yes no; do
-  ./gradlew -Pprod=$ans -Prepo=thedockguy/gohw start
+echo
+
+for ans in yes no; do
+
+  if [ "$ans" == "yes" ]; then
+    ./gradlew -Pprod=$ans -Prepo=thedockguy/gohw_prod start
+  else
+    ./gradlew -Pprod=$ans -Prepo=thedockguy/gohw_test start
+  fi
+
   echo; ls -l work; echo
+
 done
